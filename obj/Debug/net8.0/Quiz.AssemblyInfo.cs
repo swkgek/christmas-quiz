38556c3518436970dcbfd264ef25d5e4c3d0420e
Sean Kerrigan <sean.kerrigan@openreach.co.uk>
2025-12-15 11:14:41 +0000

    Add nuget config

--- obj/Debug/net8.0/Quiz.AssemblyInfo.cs
+++ obj/Debug/net8.0/Quiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a72f7d0d58b9be22983b4ec14191f6c892e2b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
